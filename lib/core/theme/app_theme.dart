ef9f1c5ba15a452c5ba6516ff7238aef7c7b49b9
germain250 <irakozemugishagermain@gmail.com>
2026-01-29 18:15:19 +0200

    changes on theme file

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -2,31 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
-  static TextStyle _baseStyle({
-    required double fontSize,
-    FontWeight fontWeight = FontWeight.w400,
-    double? letterSpacing,
-    double? height,
-    Color? color,
-  }) {
-    return GoogleFonts.plusJakartaSans(
-      fontSize: fontSize,
-      fontWeight: fontWeight,
-      letterSpacing: letterSpacing,
-      height: height,
-      color: color,
-    );
-  }
-
   static ThemeData lightTheme = ThemeData(
-    useMaterial3: true,
     brightness: Brightness.light,
     scaffoldBackgroundColor: Colors.white,
     primaryColor: Colors.black,
-    colorScheme: ColorScheme.fromSeed(
-      seedColor: Colors.black,
-      brightness: Brightness.light,
-    ),
     cardColor: Colors.grey.shade100,
 
     appBarTheme: const AppBarTheme(
@@ -36,120 +15,22 @@ class AppTheme {
     ),
 
     textTheme: TextTheme(
-      displayLarge: _baseStyle(
-        fontSize: 57,
-        fontWeight: FontWeight.w700,
-        height: 1.12,
-        letterSpacing: -0.25,
+      titleLarge: GoogleFonts.plusJakartaSans(
+        color: Colors.black,
+        fontWeight: FontWeight.bold,
       ),
-      displayMedium: _baseStyle(
-        fontSize: 45,
-        fontWeight: FontWeight.w700,
-        height: 1.16,
-        letterSpacing: 0,
-      ),
-      displaySmall: _baseStyle(
-        fontSize: 36,
-        fontWeight: FontWeight.w700,
-        height: 1.22,
-        letterSpacing: 0,
-      ),
-
-      // Headline Texts ........sections
-      headlineLarge: _baseStyle(
-        fontSize: 32,
-        fontWeight: FontWeight.w700,
-        height: 1.25,
-        letterSpacing: 0,
-      ),
-      headlineMedium: _baseStyle(
-        fontSize: 28,
-        fontWeight: FontWeight.w700,
-        height: 1.28,
-        letterSpacing: 0,
-      ),
-      headlineSmall: _baseStyle(
-        fontSize: 24,
-        fontWeight: FontWeight.w600,
-        height: 1.33,
-        letterSpacing: 0,
-      ),
-
-      // Title text ..... subsections
-      titleLarge: _baseStyle(
-        fontSize: 22,
-        fontWeight: FontWeight.w600,
-        height: 1.27,
-        letterSpacing: 0,
-      ),
-      titleMedium: _baseStyle(
-        fontSize: 16,
-        fontWeight: FontWeight.w500,
-        height: 1.50,
-        letterSpacing: 0.15,
-      ),
-      titleSmall: _baseStyle(
-        fontSize: 14,
-        fontWeight: FontWeight.w500,
-        height: 1.43,
-        letterSpacing: 0.10,
-      ),
-
-      // Body main content text
-      bodyLarge: _baseStyle(
-        fontSize: 16,
-        fontWeight: FontWeight.w400,
-        height: 1.50,
-        letterSpacing: 0.50,
-      ),
-      bodyMedium: _baseStyle(
-        fontSize: 14,
-        fontWeight: FontWeight.w400,
-        height: 1.43,
-        letterSpacing: 0.25,
-      ),
-      bodySmall: _baseStyle(
-        fontSize: 12,
-        fontWeight: FontWeight.w400,
-        height: 1.33,
-        letterSpacing: 0.40,
-      ),
-
-      // Label text ..... buttons, captions
-      labelLarge: _baseStyle(
-        fontSize: 14,
-        fontWeight: FontWeight.w500,
-        height: 1.43,
-        letterSpacing: 0.10,
-      ),
-      labelMedium: _baseStyle(
-        fontSize: 12,
-        fontWeight: FontWeight.w500,
-        height: 1.33,
-        letterSpacing: 0.50,
-      ),
-      labelSmall: _baseStyle(
-        fontSize: 11,
-        fontWeight: FontWeight.w500,
-        height: 1.45,
-        letterSpacing: 0.50,
-      ),
+      bodyMedium: GoogleFonts.plusJakartaSans(color: Colors.black87),
     ),
 
     iconTheme: const IconThemeData(color: Colors.black),
 
-    dividerColor: Colors.grey.shade300,
+    dividerColor: Colors.grey,
   );
 
   static ThemeData darkTheme = ThemeData(
-    useMaterial3: true,
     brightness: Brightness.dark,
     scaffoldBackgroundColor: Colors.black,
     primaryColor: Colors.white,
-    colorScheme: ColorScheme.fromSeed(
-      seedColor: Colors.white,
-      brightness: Brightness.dark,
-    ),
     cardColor: Colors.grey.shade900,
 
     appBarTheme: const AppBarTheme(
@@ -159,122 +40,15 @@ class AppTheme {
     ),
 
     textTheme: TextTheme(
-      displayLarge: _baseStyle(
-        fontSize: 57,
-        fontWeight: FontWeight.w700,
-        height: 1.12,
-        letterSpacing: -0.25,
+      titleLarge: GoogleFonts.plusJakartaSans(
         color: Colors.white,
+        fontWeight: FontWeight.bold,
       ),
-      displayMedium: _baseStyle(
-        fontSize: 45,
-        fontWeight: FontWeight.w700,
-        height: 1.16,
-        letterSpacing: 0,
-        color: Colors.white,
-      ),
-      displaySmall: _baseStyle(
-        fontSize: 36,
-        fontWeight: FontWeight.w700,
-        height: 1.22,
-        letterSpacing: 0,
-        color: Colors.white,
-      ),
-
-      // Headline
-      headlineLarge: _baseStyle(
-        fontSize: 32,
-        fontWeight: FontWeight.w700,
-        height: 1.25,
-        letterSpacing: 0,
-        color: Colors.white,
-      ),
-      headlineMedium: _baseStyle(
-        fontSize: 28,
-        fontWeight: FontWeight.w700,
-        height: 1.28,
-        letterSpacing: 0,
-        color: Colors.white,
-      ),
-      headlineSmall: _baseStyle(
-        fontSize: 24,
-        fontWeight: FontWeight.w600,
-        height: 1.33,
-        letterSpacing: 0,
-        color: Colors.white,
-      ),
-
-      titleLarge: _baseStyle(
-        fontSize: 22,
-        fontWeight: FontWeight.w600,
-        height: 1.27,
-        letterSpacing: 0,
-        color: Colors.white,
-      ),
-      titleMedium: _baseStyle(
-        fontSize: 16,
-        fontWeight: FontWeight.w500,
-        height: 1.50,
-        letterSpacing: 0.15,
-        color: Colors.white70,
-      ),
-      titleSmall: _baseStyle(
-        fontSize: 14,
-        fontWeight: FontWeight.w500,
-        height: 1.43,
-        letterSpacing: 0.10,
-        color: Colors.white70,
-      ),
-
-      // Body
-      bodyLarge: _baseStyle(
-        fontSize: 16,
-        fontWeight: FontWeight.w400,
-        height: 1.50,
-        letterSpacing: 0.50,
-        color: Colors.white70,
-      ),
-      bodyMedium: _baseStyle(
-        fontSize: 14,
-        fontWeight: FontWeight.w400,
-        height: 1.43,
-        letterSpacing: 0.25,
-        color: Colors.white70,
-      ),
-      bodySmall: _baseStyle(
-        fontSize: 12,
-        fontWeight: FontWeight.w400,
-        height: 1.33,
-        letterSpacing: 0.40,
-        color: Colors.white60,
-      ),
-
-      // Label
-      labelLarge: _baseStyle(
-        fontSize: 14,
-        fontWeight: FontWeight.w500,
-        height: 1.43,
-        letterSpacing: 0.10,
-        color: Colors.white,
-      ),
-      labelMedium: _baseStyle(
-        fontSize: 12,
-        fontWeight: FontWeight.w500,
-        height: 1.33,
-        letterSpacing: 0.50,
-        color: Colors.white70,
-      ),
-      labelSmall: _baseStyle(
-        fontSize: 11,
-        fontWeight: FontWeight.w500,
-        height: 1.45,
-        letterSpacing: 0.50,
-        color: Colors.white60,
-      ),
+      bodyMedium: GoogleFonts.plusJakartaSans(color: Colors.white70),
     ),
 
     iconTheme: const IconThemeData(color: Colors.white),
 
-    dividerColor: Colors.grey.shade800,
+    dividerColor: Colors.grey,
   );
 }
