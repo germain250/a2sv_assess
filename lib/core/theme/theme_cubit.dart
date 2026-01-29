9a9d851cf62544c8abdbe36e215f0aa14e7ad100
germain250 <irakozemugishagermain@gmail.com>
2026-01-29 21:43:12 +0200

    Added theme persistence

--- lib/core/theme/theme_cubit.dart
+++ lib/core/theme/theme_cubit.dart
@@ -1,9 +1,24 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeCubit extends Cubit<bool> {
-  ThemeCubit() : super(false);
+  static const String _themeKey = 'isDarkMode';
 
-  void toggleTheme() {
-    emit(!state);
+  ThemeCubit() : super(false) {
+    _loadTheme();
+  }
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final savedIsDark = prefs.getBool(_themeKey) ?? false;
+    emit(savedIsDark);
+  }
+
+  Future<void> toggleTheme() async {
+    final newIsDark = !state;
+
+    emit(newIsDark);
+
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_themeKey, newIsDark);
   }
 }
